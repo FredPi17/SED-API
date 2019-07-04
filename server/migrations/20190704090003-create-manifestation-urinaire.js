ece0528145e6f7eb4e16be3a8db2c711fcb96166
Frédéric <fpinaud17@gmail.com>
2019-07-04 11:06:28 +0200

    Oups.. Missed to add two files on Git

---
+++ server/migrations/20190704090003-create-manifestation-urinaire.js
@@ -0,0 +1,132 @@
+'use strict';
+module.exports = {
+    up: (queryInterface, Sequelize) => {
+        return queryInterface.createTable('ManifestationUrinaires', {
+            id: {
+                allowNull: false,
+                autoIncrement: true,
+                primaryKey: true,
+                type: Sequelize.INTEGER
+            },
+            bruluresMictionnelle: {
+                type: Sequelize.BOOLEAN
+            },
+            douleurFosseLombaire: {
+                type: Sequelize.BOOLEAN
+            },
+            infectionUrinaireSuspectee: {
+                type: Sequelize.BOOLEAN
+            },
+            infectionUrinaireAveree: {
+                type: Sequelize.BOOLEAN
+            },
+            pyelonephrite: {
+                type: Sequelize.BOOLEAN
+            },
+            coliqueNephretique: {
+                type: Sequelize.BOOLEAN
+            },
+            BULeucocyte: {
+                type: Sequelize.STRING
+            },
+            BUNitrite: {
+                type: Sequelize.STRING
+            },
+            BUSang: {
+                type: Sequelize.STRING
+            },
+            BUProteine: {
+                type: Sequelize.STRING
+            },
+            BUGlucose: {
+                type: Sequelize.STRING
+            },
+            BUBilirubine: {
+                type: Sequelize.STRING
+            },
+            BUUrobilinogene: {
+                type: Sequelize.STRING
+            },
+            BUCetone: {
+                type: Sequelize.STRING
+            },
+            BUDensite: {
+                type: Sequelize.STRING
+            },
+            BUPH: {
+                type: Sequelize.STRING
+            },
+            BUAcideAscorbique: {
+                type: Sequelize.STRING
+            },
+            douleurVessie: {
+                type: Sequelize.BOOLEAN
+            },
+            hematurieMacro: {
+                type: Sequelize.BOOLEAN
+            },
+            hematurieMicro: {
+                type: Sequelize.BOOLEAN
+            },
+            urineTrouble: {
+                type: Sequelize.BOOLEAN
+            },
+            urineMalodorante: {
+                type: Sequelize.BOOLEAN
+            },
+            nycturie: {
+                type: Sequelize.BOOLEAN
+            },
+            fuiteUrinaire: {
+                type: Sequelize.BOOLEAN
+            },
+            incontinenceEffort: {
+                type: Sequelize.BOOLEAN
+            },
+            imperiosite: {
+                type: Sequelize.BOOLEAN
+            },
+            gouttesRetardataire: {
+                type: Sequelize.BOOLEAN
+            },
+            diminutionBesoinUriner: {
+                type: Sequelize.BOOLEAN
+            },
+            retentionUrinaire: {
+                type: Sequelize.BOOLEAN
+            },
+            difficulteUriner: {
+                type: Sequelize.BOOLEAN
+            },
+            impossibiliteUriner: {
+                type: Sequelize.BOOLEAN
+            },
+            sensationVidangeComplete: {
+                type: Sequelize.BOOLEAN
+            },
+            mictionDeuxTemps: {
+                type: Sequelize.BOOLEAN
+            },
+            mictionPoussee: {
+                type: Sequelize.BOOLEAN
+            },
+            commentaire: {
+                type: Sequelize.STRING
+            },
+            dateDuJour: {
+                type: Sequelize.DATE
+            },
+            createdAt: {
+                allowNull: false,
+                type: Sequelize.DATE
+            },
+            updatedAt: {
+                allowNull: false,
+                type: Sequelize.DATE
+            }
+        });
+    },
+    down: (queryInterface, Sequelize) => {
+        return queryInterface.dropTable('ManifestationUrinaires');
+    }
+};
---
+++ server/models/manifestationurinaire.js
@@ -0,0 +1,45 @@
+'use strict';
+module.exports = (sequelize, DataTypes) => {
+    const ManifestationUrinaire = sequelize.define('ManifestationUrinaire', {
+        bruluresMictionnelle: DataTypes.BOOLEAN,
+        douleurFosseLombaire: DataTypes.BOOLEAN,
+        infectionUrinaireSuspectee: DataTypes.BOOLEAN,
+        infectionUrinaireAveree: DataTypes.BOOLEAN,
+        pyelonephrite: DataTypes.BOOLEAN,
+        coliqueNephretique: DataTypes.BOOLEAN,
+        BULeucocyte: DataTypes.STRING,
+        BUNitrite: DataTypes.STRING,
+        BUSang: DataTypes.STRING,
+        BUProteine: DataTypes.STRING,
+        BUGlucose: DataTypes.STRING,
+        BUBilirubine: DataTypes.STRING,
+        BUUrobilinogene: DataTypes.STRING,
+        BUCetone: DataTypes.STRING,
+        BUDensite: DataTypes.STRING,
+        BUPH: DataTypes.STRING,
+        BUAcideAscorbique: DataTypes.STRING,
+        douleurVessie: DataTypes.BOOLEAN,
+        hematurieMacro: DataTypes.BOOLEAN,
+        hematurieMicro: DataTypes.BOOLEAN,
+        urineTrouble: DataTypes.BOOLEAN,
+        urineMalodorante: DataTypes.BOOLEAN,
+        nycturie: DataTypes.BOOLEAN,
+        fuiteUrinaire: DataTypes.BOOLEAN,
+        incontinenceEffort: DataTypes.BOOLEAN,
+        imperiosite: DataTypes.BOOLEAN,
+        gouttesRetardataire: DataTypes.BOOLEAN,
+        diminutionBesoinUriner: DataTypes.BOOLEAN,
+        retentionUrinaire: DataTypes.BOOLEAN,
+        difficulteUriner: DataTypes.BOOLEAN,
+        impossibiliteUriner: DataTypes.BOOLEAN,
+        sensationVidangeComplete: DataTypes.BOOLEAN,
+        mictionDeuxTemps: DataTypes.BOOLEAN,
+        mictionPoussee: DataTypes.BOOLEAN,
+        commentaire: DataTypes.STRING,
+        dateDuJour: DataTypes.DATE
+    }, {});
+    ManifestationUrinaire.associate = function (models) {
+        // associations can be defined here
+    };
+    return ManifestationUrinaire;
+};
