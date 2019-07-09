121ea64bbc987e20ccca06d2d8de46e2fd822d04
Frédéric <fpinaud17@gmail.com>
2019-07-09 17:16:40 +0200

    Modifications on app.js

--- app.js
+++ app.js
@@ -16,6 +16,7 @@ app.use(bodyParser.urlencoded({extended: false}));
 app.use(function (req, res, next) {
     res.header("Access-Control-Allow-Origin", "*");
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    res.header("Accept-Control-Allow-Method", 'POST, GET, PUT, DELETE');
     next();
 });
 routes(app);
